14a004c58d314d4e3e0c1a0e20291f580240dfd3
seksit39 <seksit39@outlook.com>
2016-12-07 15:53:45 +0700

    Add files via upload
    starter

---
+++ ASK.ino
@@ -0,0 +1,24 @@
+d#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+#define pi = 3.14159265;
+#define frequency = 100;
+
+Adafruit_MCP4725 dac;
+
+float amplitude = 0.5;
+
+void setup(void) {
+  
+  Serial.begin(9600);
+  dac.begin(0x62);      // is default
+    
+}
+
+void loop(void) {
+
+      int ask = 1024*(0.5+(amplitude*sin(2*pi*frequency*micro()/1000000)));
+
+      dac.setVoltage(ask, false);
+
+}
