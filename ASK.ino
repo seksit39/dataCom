10fbe82233e236757be7b8547a833c98a63567c8
seksit39 <seksit39@outlook.com>
2016-12-12 21:03:40 +0700

    Add files via upload

--- ASK.ino
+++ ASK.ino
@@ -1,12 +1,24 @@
+
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 
 #define pi 3.14159265
-#define frequency  100
+#define frequency  60
 
 Adafruit_MCP4725 dac;
 
 float amplitude = 0.5;
+int multipi = 1;
+int del = 20;
+int input[10] = {0,0,0,0,0,0,0,0,0,0};
+
+const uint16_t sine[32] =
+{
+  2048, 2447, 2831, 3185, 3495, 3750, 3939, 4056,
+  4095, 4056, 3939, 3750, 3495, 3185, 2831, 2447,
+  2048, 1648, 1264,  910,  600,  345,  156,   39,
+     0,   39,  156,  345,  600,  910, 1264, 1648
+};
 
 void setup(void) {
   
@@ -17,8 +29,18 @@ void setup(void) {
 
 void loop(void) {
 
-      int ask = 1024*(0.5+(amplitude*sin(2*pi*frequency*micros()/1000000)));
-
-      dac.setVoltage(ask, false);
-
+      if(Serial.available() != 0){
+        int tmp = Serial.parseInt();
+        if(tmp > 0){
+          multipi = tmp;
+          Serial.println(multipi);
+        } 
+      }
+    for(int a=1 ; a<5   ; a++){
+      for(int b=0 ; b<2 ; b++){
+          for(int i=0 ; i<32 ; i++){
+            dac.setVoltage(sine[i]*a/4, false);
+          }
+      }
+    }
 }
