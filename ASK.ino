87743a16272f1c8341b76d42c935c4e4d4f3337f
seksit39 <seksit39@outlook.com>
2016-12-13 00:15:42 +0700

    Update ASK.ino
    Sender

--- ASK.ino
+++ ASK.ino
@@ -4,13 +4,15 @@ uncomment sender or reciever to select
 */
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
+#include <Adafruit_ADS1015.h>
 
 //#define SENDER
 //#define RECIEVER
 
+Adafruit_ADS1115 ads;
 Adafruit_MCP4725 dac;
-int a = 0;
-int input[10] = {0,0,0,0,0,0,0,0,0,0};
+uint16_t a = 0;
+uint16_t input[10] = {0,0,0,0,0,0,0,0,0,0};
 
 const uint16_t sine[32] =
 {
@@ -23,45 +25,45 @@ const uint16_t sine[32] =
 void setup(void) {
   
   Serial.begin(9600);
-  dac.begin(0x62);      // is default
-    
+  dac.begin(0x62);
+  ads.begin();  
 }
 
 void loop(void) {
 #ifdef SEND
       if(Serial.available() != 0){
-        int tmp = Serial.parseInt();
-        if(tmp > 0){
-          multipi = tmp;
-          Serial.println(multipi);
+        for(int i=0 ; i<10 ; i++){
+          input[i] = Serial.parseInt(); 
+          Serial.parseInt();
         } 
+        for(int k=0 ; k<10 ; k+=2){
+          if(input[k] == 0){
+            if(input[k+1] == 0){
+              a = 1;
+            }
+            else if(input[k+1] == 1){
+              a = 2;
+            }
+          }
+          else if(input[k] == 1){
+            if(input[k+1] == 0){
+              a = 3;
+            }
+            else if(input[k+1] == 1){
+              a = 4;
+            }
+          }
+          for(int j=0 ; j<4 ; j++){
+              for(int i=0 ; i<32 ; i++){
+                dac.setVoltage(sine[i]*a/4, false);
+              }
+          }
+        }   
       }
   
-    for(int k=0 ; k<10 ; k+=2){
-      if(input[k] == 0){
-        if(input[k+1] == 0){
-          a = 1;
-        }
-        else if(input[k+1] == 1){
-          a = 2;
-        }
-      }
-      else if(input[k] == 1){
-        if(input[k+1] == 0){
-          a = 3;
-        }
-        else if(input[k+1] == 1){
-          a = 4;
-        }
-      }
-      for(int j=0 ; j<4 ; j++){
-          for(int i=0 ; i<32 ; i++){
-            dac.setVoltage(sine[i]*a/4, false);
-          }
-      }
-    }
+
 #endif
 #ifdef RECIEVER
-  
+  uint16_t tmp = ads.readADC_SingleEnded(0);
 #endif
 }
