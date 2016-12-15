b7f136c16346cf72ed174472829bf014a478fc85
seksit39 <seksit39@outlook.com>
2016-12-15 15:46:29 +0700

    Add files via upload

---
+++ SpeedADC.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+// Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  
+  ads.begin();
+  int first = millis();
+  int cout = 0;
+  while(millis()-first < 1000){
+    int adc0 = ads.readADC_SingleEnded(0);
+    cout++;
+  }
+  Serial.print(cout);
+}
+
+void loop(void) 
+{
+  
+
+  
+  
+  
+}
