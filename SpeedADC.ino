af2fd3647ddbd011f574ea5c89b36d24bfb2243b
seksit39 <seksit39@outlook.com>
2016-12-23 23:58:01 +0700

    Update SpeedADC.ino

--- SpeedADC.ino
+++ SpeedADC.ino
@@ -6,6 +6,36 @@ Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
 void setup(void) 
 {
+  
+  // defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+// set prescale to 16
+cbi(ADCSRA,ADPS2) ;
+cbi(ADCSRA,ADPS1) ;
+cbi(ADCSRA,ADPS0) ;
+
+  Serial.begin(115200);
+
+  long first = micros();
+  long cout = 0;
+  while(micros()-first < 1000000){
+    int temp = analogRead(A0);
+    cout++;
+  }
+  
+  Serial.print(cout);
+  Serial.print(" Hz");
+
+}
+/*
+void setup(void) 
+{
   Serial.begin(9600);
   
   ads.begin();
@@ -17,7 +47,7 @@ void setup(void)
   }
   Serial.print(cout);
 }
-
+*/
 void loop(void) 
 {
   
